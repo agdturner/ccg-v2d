594d343a0d0afa41100ecd4e5ab9839a0f6edc2e
geoagdt <geoagdt@GEO-L10624>
2017-10-30 08:39:59 +0000

    Created Generic Environment Strings and Files for extension.

--- src/main/java/uk/ac/leeds/ccg/andyt/vector/grids/Vector_LineGrid.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/vector/grids/Vector_LineGrid.java
@@ -14,7 +14,7 @@ import uk.ac.leeds.ccg.andyt.grids.core.grid.chunk.Grids_GridChunkDoubleArrayFac
 import uk.ac.leeds.ccg.andyt.grids.core.grid.Grids_GridDoubleFactory;
 import uk.ac.leeds.ccg.andyt.grids.core.statistics.Grids_GridStatistics0;
 import uk.ac.leeds.ccg.andyt.grids.core.Grids_Environment;
-import uk.ac.leeds.ccg.andyt.grids.exchange.Grids_ImageExporter;
+import uk.ac.leeds.ccg.andyt.grids.io.Grids_ImageExporter;
 import uk.ac.leeds.ccg.andyt.grids.process.Grids_Processor;
 import uk.ac.leeds.ccg.andyt.vector.core.Vector_Environment;
 import uk.ac.leeds.ccg.andyt.vector.core.Vector_Object;
